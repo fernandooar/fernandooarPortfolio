0ed9e763644abd9115254b21e9a6a9e3a7be1f1b
FERNANDO DE OLIVEIRA ALMEIDA <118127229+fernandooar@users.noreply.github.com>
2023-03-23 21:33:08 -0300

    Atualização de layout responsivo
    Atualização do layout responsivo e implementação de scripts JS

--- jscript/jscript.js
+++ jscript/jscript.js
@@ -5,3 +5,19 @@ hamburguer.addEventListener("click", () => {
   hamburguer.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
+
+
+function typeWriter(el){
+  const mainText =el.innerHTML.split("")
+  el.innerHTML = ''
+
+  mainText.forEach(function(latter,i) {
+    setTimeout(function(){
+      el.innerHTML += latter
+    }, 60*i)
+    
+  });
+}
+
+const phrase = document.querySelector('.mainSentence')
+typeWriter(phrase)
